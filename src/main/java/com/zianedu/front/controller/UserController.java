598b303606df36e2f0d82afebacb2449fe7c86b0
anjiho <anjo0080@gmail.com>
2019-07-30 13:43:16 +0900

    ./

--- src/main/java/com/zianedu/front/controller/UserController.java
+++ src/main/java/com/zianedu/front/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.zianedu.front.controller;
 
+import com.zianedu.front.utils.Util;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,13 @@ public class UserController {
     @RequestMapping(value = "/user")
     public ModelAndView userManage(@RequestParam(value = "page_gbn", required = false) String page_gbn) {
         ModelAndView mvc = new ModelAndView();
+        page_gbn = Util.isNullValue(page_gbn, "");
+
+        if ("joinAgree".equals(page_gbn)) {
+            mvc.setViewName("post/online/main");
+        } else {
+
+        }
 
         return mvc;
     }
