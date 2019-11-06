6e52e5d8157d9993f2ca6c91b2b8a698c0811cb8
안지호 <anjo0080@gmail.com>
2019-11-06 09:57:09 +0900

    공지사항 컨트롤러 연결

---
+++ src/main/java/com/zianedu/front/controller/NoticeController.java
@@ -0,0 +1,30 @@
+package com.zianedu.front.controller;
+
+import com.zianedu.front.utils.Util;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 계리직 학원 컨트롤러
+ */
+@Controller
+public class NoticeController {
+
+    @RequestMapping(value = "/notice")
+    public ModelAndView postAcademyManage(@RequestParam(value = "page_gbn", required = false) String page_gbn) {
+        ModelAndView mvc = new ModelAndView();
+
+        page_gbn = Util.isNullValue(page_gbn, "");
+
+        if ("list".equals(page_gbn)) {
+            mvc.setViewName("/notice/list");
+        } else if ("detail".equals(page_gbn)) {
+            mvc.setViewName("/notice/detail");
+        } else if ("write".equals(page_gbn)) {
+            mvc.setViewName("/notice/write");
+        }
+        return mvc;
+    }
+}
