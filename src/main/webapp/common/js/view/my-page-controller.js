6edab3dc35ae0b712d5b240272218660df79e396
woneunjoeng <huuc10@gmail.com>
2020-02-11 11:27:18 +0900

    ./

--- src/main/webapp/common/js/view/my-page-controller.js
+++ src/main/webapp/common/js/view/my-page-controller.js
@@ -95,7 +95,7 @@ function getVideoSignUpDetailInfo(gkey, device, jlecKey, tagId) {
             function(data) {return data.numStr;},
             function(data) {return data.name;},
             function(data) {return data.vodTime;},
-            function(data) {return '<a href="javascript:void(0);" onclick="OpenLecPlayer(' + "'"  + data.vodFileLow + "'" + ',' + "'"  + data.vodFileLow + "'" + ');" class="black small">일반화질</a>&nbsp;&nbsp;<a href="javascript:void(0);" onclick="OpenLecPlayer(' + "'"  + data.vodFileHigh + "'" + ',' + "'"  + data.vodFileLow + "'" + ');" class="blue small">고화질</a>';}
+            function(data) {return '<a href="javascript:void(0);" onclick="OpenLecPlayer(' + "'"  + data.vodFileLow + "'" + ',' + "'"  + data.name + "'" + ');" class="black small">일반화질</a>&nbsp;&nbsp;<a href="javascript:void(0);" onclick="OpenLecPlayer(' + "'"  + data.vodFileHigh + "'" + ',' + "'"  + data.name + "'" + ');" class="blue small">고화질</a>';}
         ], {escapeHtml:false});
     }
     return detailInfo;
