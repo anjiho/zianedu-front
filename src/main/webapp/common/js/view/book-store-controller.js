12eddf509e8e6e9c2c464209af4e2af00648b6be
woneunjoeng <huuc10@gmail.com>
2020-03-12 11:10:14 +0900

    ./

--- src/main/webapp/common/js/view/book-store-controller.js
+++ src/main/webapp/common/js/view/book-store-controller.js
@@ -6,7 +6,7 @@ function getNewBookList() {
         for(var i=0; i<selList.length;i++){
             var returnHtml = "<div class=\"item\"><div>";
             returnHtml += "<a href='javascript:goDetailBook("+ selList[i].gkey +");'><img src='"+ selList[i].bookImageUrl +"' alt=\"\"></a>";
-            returnHtml += "<span class=\"name\">"+ selList[i].goodsName +"</span></a>";
+            returnHtml += "<a href='javascript:goDetailBook("+ selList[i].gkey +");'><span class=\"name\">"+ selList[i].goodsName +"</span></a>";
             if(selList[i].name != null){
                 returnHtml += "<span class=\"writer\">"+ selList[i].writer + " | "+ selList[i].name +"</span>";
             }else{
@@ -32,7 +32,7 @@ function getBestBookList() {
         for(var i=0; i<selList.length;i++){
             var returnHtml = "<div class=\"item\"><div>";
             returnHtml += "<a href='javascript:goDetailBook("+ selList[i].gkey +");'><img src='"+ selList[i].bookImageUrl +"' alt=\"\"></a>";
-            returnHtml += "<span class=\"name\">"+ selList[i].goodsName +"</span>";
+            returnHtml += "<a href='javascript:goDetailBook("+ selList[i].gkey +");'><span class=\"name\">"+ selList[i].goodsName +"</span></a>";
             if(selList[i].name != null){
                 returnHtml += "<span class=\"writer\">"+ selList[i].writer + " | "+ selList[i].name +"</span>";
             }else{
